487e54a9ce16a74b3b7192e9f8ad0ac8893d3f82
Masked-Kunsiquat <130736043+Masked-Kunsiquat@users.noreply.github.com>
2025-01-15 04:09:37 -0500

    making ammendments to hopefully read the correct directory (`website`)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,20 +10,21 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'HomeLab',
+  tagline: 'Self-hosting is cool!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://masked-kunsiquat.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/HomeLab/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Masked-Kunsiquat', // Usually your GitHub org/user name.
+  projectName: 'HomeLab', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
